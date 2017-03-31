37f4c13f62afacad591bcc55dd25df250fc3f310
Tim <tmthyndrsn@gmail.com>
2017-03-31 09:51:41 -0700

    Added basic server code for receiving/sending message

--- server/server.py
+++ server/server.py
@@ -1,9 +1,22 @@
 
-setup server
+"""setup server
 establish socket connection to client
 
 loop for requests
     iWant filename from client
         check that command is correct format (send error if no)
         check if file exists (send error if no)
-        send file
+        send file"""
+
+from socket import *
+
+
+serverPort = 12000
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+serverSocket.bind(('', serverPort))
+print "The server is ready to receive"
+
+while 1:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    modifiedMessage = message.upper()
+    serverSocket.sendto(modifiedMessage, clientAddress)
