1804c4565a14af2fe10722bf5f49e35073733865
Daniel McGarry <mcgarrdg@mcgarrdglaptop.rose-hulman.edu>
2017-03-31 13:44:18 -0400

    basic iWant done

--- server/server.py
+++ server/server.py
@@ -9,6 +9,7 @@ loop for requests
         send file"""
 
 from socket import *
+import os.path
 
 
 serverPort = 12000
@@ -18,12 +19,19 @@ print "The server is ready to receive"
 
 while 1:
     message, clientAddress = serverSocket.recvfrom(2048)
-    if (message.startsWith("iWant "):
+    if (message[:6] == ("iWant ")):
         filename = message[6:]
-        check that command is correct format (send error if no)
-        check if file exists (send error if no)
-        send file
+        path = "./store/"+filename
+        if (len(filename.split(" ")) != 1 or not filename): 
+        	serverSocket.sendto("That just ain't right!", clientAddress)
+        elif not os.path.isfile(path):
+        	serverSocket.sendto("Failure: What you talkin' bout Willis? I ain't seen that file nowhere!", clientAddress)
+        else: 
+        	f = open(path, 'rb')
+        	data= f.read()
+        	f.close()
+        	serverSocket.sendto(data, clientAddress)
+    else: 
+    	serverSocket.sendto("That just ain't right!", clientAddress)
 
 
-    modifiedMessage = message.upper()
-    serverSocket.sendto(modifiedMessage, clientAddress)
