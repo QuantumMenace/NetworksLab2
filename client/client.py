600615d06be3729a3767b4b53e6cc0bf969555a2
Daniel McGarry <mcgarrdg@mcgarrdglaptop.rose-hulman.edu>
2017-03-31 15:12:16 -0400

    files thus far

--- client/client.py
+++ client/client.py
@@ -13,11 +13,45 @@ loop for input on command line
         send file when server is ready"""
 
 from socket import *
+import os.path 
+
 serverName = '127.0.0.1' 
 serverPort = 12000
 clientSocket = socket(AF_INET, SOCK_DGRAM)
-message = raw_input('Input lowercase sentence:')
-clientSocket.sendto(message, (serverName, serverPort))
-modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
-print modifiedMessage
+while 1:
+	message = raw_input('> ')
+	if (message[:6] == ("iWant ")):
+		clientSocket.sendto(message, (serverName, serverPort))
+		modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+		if (not modifiedMessage): 
+			print "receiving file"
+			location =  raw_input('Input directory or enter>')
+			path = "./received/"
+			if (location != ""):
+				path = location + "/"
+			path = path + message[6:] 
+			with open(path, "w+") as f:
+				modifiedMessage = " "
+				while modifiedMessage:
+					modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+					f.write(modifiedMessage)
+		else: 
+			print modifiedMessage
+
+	elif (message[:6] == ("uTake ")):
+		path = message[6:]
+		if not os.path.isfile(path):
+			print "Failure: What you talkin' bout Willis? I ain't seen that file nowhere!"
+		else:	
+			clientSocket.sendto(message, (serverName, serverPort))
+			with open(path, 'rb') as f:
+				data = f.read(2048)
+				while data:
+					clientSocket.sendto(data, (serverName, serverPort))
+					data = f.read(2048)
+			clientSocket.sendto("", (serverName, serverPort))
+	else:
+		clientSocket.sendto(message, (serverName, serverPort))
+
+print "Client Closing"
 clientSocket.close()
--- server/server.py
+++ server/server.py
@@ -3,10 +3,10 @@
 establish socket connection to client
 
 loop for requests
-    iWant filename from client
-        check that command is correct format (send error if no)
-        check if file exists (send error if no)
-        send file"""
+	iWant filename from client
+		check that command is correct format (send error if no)
+		check if file exists (send error if no)
+		send file"""
 
 from socket import *
 import os.path
@@ -18,20 +18,34 @@ serverSocket.bind(('', serverPort))
 print "The server is ready to receive"
 
 while 1:
-    message, clientAddress = serverSocket.recvfrom(2048)
-    if (message[:6] == ("iWant ")):
-        filename = message[6:]
-        path = "./store/"+filename
-        if (len(filename.split(" ")) != 1 or not filename): 
-        	serverSocket.sendto("That just ain't right!", clientAddress)
-        elif not os.path.isfile(path):
-        	serverSocket.sendto("Failure: What you talkin' bout Willis? I ain't seen that file nowhere!", clientAddress)
-        else: 
-        	f = open(path, 'rb')
-        	data= f.read()
-        	f.close()
-        	serverSocket.sendto(data, clientAddress)
-    else: 
-    	serverSocket.sendto("That just ain't right!", clientAddress)
+	message, clientAddress = serverSocket.recvfrom(2048)
+	if (message[:6] == ("iWant ")):
+		filename = message[6:]
+		path = "./store/"+filename
+		if (len(filename.split(" ")) != 1 or not filename): 
+			serverSocket.sendto("That just ain't right!", clientAddress)
+		elif not os.path.isfile(path):
+			serverSocket.sendto("Failure: What you talkin' bout Willis? I ain't seen that file nowhere!", clientAddress)
+		else: 
+			serverSocket.sendto("", clientAddress)
+			with open(path, 'rb') as f:
+				data = f.read(2048)
+				while data:
+					serverSocket.sendto(data, clientAddress)
+					data = f.read(2048)
+				print "done sending data"
+				serverSocket.sendto("", clientAddress)
+	elif (message[:6] == ("uTake ")):
+		if (not modifiedMessage): 
+			print "receiving file"
+			path = "./store/"
+			path = path + message[6:] 
+			with open(path, "w+") as f:
+				modifiedMessage = " "
+				while modifiedMessage:
+					modifiedMessage, clientAddress = serverSocket.recvfrom(2048)
+					f.write(modifiedMessage)
+	else:
+		serverSocket.sendto("That just ain't right!", clientAddress)
 
 
