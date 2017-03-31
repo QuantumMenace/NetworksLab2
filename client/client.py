0b1d2850386c2a7c0107e1fe7cbc34900fd7c918
Daniel McGarry <mcgarrdg@mcgarrdglaptop.rose-hulman.edu>
2017-03-31 12:53:26 -0400

    basicclient

--- client/client.py
+++ client/client.py
@@ -1,5 +1,5 @@
 
-setup client
+"""setup client
 establish socket to server
 
 loop for input on command line
@@ -10,4 +10,14 @@ loop for input on command line
     uTake filename
         send request to server
         client checks if file exists
-        send file when server is ready
+        send file when server is ready"""
+
+from socket import *
+serverName = 'hostname' 
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = raw_input('Input lowercase sentence:')
+clientSocket.sendto(message, (serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvform(2048)
+print modifiedMessage
+clientSocekt.close()
